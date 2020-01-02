4d756a7467c08878d6475e6adf380c406ac2a116
chonjessica23 <33145180+chonjessica23@users.noreply.github.com>
2020-01-02 18:09:57 -0500

    Add files via upload

---
+++ comics/js/script.js
@@ -0,0 +1,221 @@
+// this is for breads
+function hover1() {
+	document.getElementById("bread1").style.display = "block"
+}
+function noHover1() {
+	document.getElementById("bread1").style.display = "none"
+}
+
+		function hover2() {
+			document.getElementById("bread2").style.display = "block"
+		}
+
+		function noHover2() {
+			document.getElementById("bread2").style.display = "none"
+		}
+				
+				function hover3() {
+					document.getElementById("bread3").style.display = "block"
+				}
+
+				function noHover3() {
+					document.getElementById("bread3").style.display = "none"
+				}
+
+						function hover4() {
+							document.getElementById("bread4").style.display = "block"
+						}
+
+						function noHover4() {
+							document.getElementById("bread4").style.display = "none"
+						}
+
+function onToasterClick (){
+	document.getElementById("imageID").style.display = "block"
+}
+// this is for spreads
+
+function hover5() {
+	document.getElementById("spread1").style.display = "block"
+}
+
+function noHover5() {
+	document.getElementById("spread1").style.display = "none"
+}
+
+		function hover6() {
+			document.getElementById("spread2").style.display = "block"
+		}
+
+		function noHover6() {
+			document.getElementById("spread2").style.display = "none"
+		}
+						
+				function hover7() {
+					document.getElementById("spread3").style.display = "block"
+				}
+
+				function noHover7() {
+					document.getElementById("spread3").style.display = "none"
+				}
+
+						function hover8() {
+							document.getElementById("spread4").style.display = "block"
+						}
+								
+						function noHover8() {
+							document.getElementById("spread4").style.display = "none"
+						}
+
+//spreads
+function onMayoClick() {
+	document.getElementById("imageID1").style.display = "block"
+}
+
+function onMustardClick() {
+	document.getElementById("imageID2").style.display = "block"
+}
+
+function onJellyClick() {
+	document.getElementById("imageID3").style.display = "block"
+}
+
+function onPeanutbutterClick() {
+	document.getElementById("imageID4").style.display = "block"
+}
+
+
+// this is for cheeses
+function hover9() {
+	document.getElementById("cheese1").style.display = "block"
+}
+
+function noHover9() {
+	document.getElementById("cheese1").style.display = "none"
+}
+
+		function hover10() {
+			document.getElementById("cheese2").style.display = "block"
+		}
+
+		function noHover10() {
+			document.getElementById("cheese2").style.display = "none"
+		}
+						
+				function hover11() {
+					document.getElementById("cheese3").style.display = "block"
+				}
+
+				function noHover11() {
+					document.getElementById("cheese3").style.display = "none"
+				}
+
+						function hover12() {
+							document.getElementById("cheese4").style.display = "block"
+						}
+								
+						function noHover12() {
+							document.getElementById("cheese4").style.display = "none"
+						}
+
+function onAmericanClick(){
+	document.getElementById("imageID5").style.display = "block"
+}
+
+function onPepperjackClick(){
+	document.getElementById("imageID6").style.display = "block"
+}
+
+function onSwissClick(){
+	document.getElementById("imageID7").style.display = "block"
+}
+
+function onMozzarellaClick(){
+	document.getElementById("imageID8").style.display = "block"
+}
+
+// this is for veg
+function hover13() {
+	document.getElementById("veg1").style.display = "block"
+}
+
+function noHover13() {
+	document.getElementById("veg1").style.display = "none"
+}
+
+		function hover14() {
+			document.getElementById("veg2").style.display = "block"
+		}
+
+		function noHover14() {
+			document.getElementById("veg2").style.display = "none"
+		}
+						
+				function hover15() {
+					document.getElementById("veg3").style.display = "block"
+				}
+
+				function noHover15() {
+					document.getElementById("veg3").style.display = "none"
+				}
+
+function onTomatoClick() {
+	document.getElementById("imageID9").style.display = "block"
+}
+
+function onOnionClick() {
+	document.getElementById("imageID10").style.display = "block"
+}
+
+function onLettuceClick() {
+	document.getElementById("imageID11").style.display = "block"
+}
+
+// this is for fill
+function hover16() {
+	document.getElementById("fill1").style.display = "block"
+}
+
+function noHover16() {
+	document.getElementById("fill1").style.display = "none"
+}
+
+		function hover17() {
+			document.getElementById("fill2").style.display = "block"
+		}
+
+		function noHover17() {
+			document.getElementById("fill2").style.display = "none"
+		}
+						
+				function hover18() {
+					document.getElementById("fill3").style.display = "block"
+				}
+
+				function noHover18() {
+					document.getElementById("fill3").style.display = "none"
+				}
+
+						function hover19() {
+							document.getElementById("fill4").style.display = "block"
+						}
+								
+						function noHover19() {
+							document.getElementById("fill4").style.display = "none"
+						}
+
+function onChickenClick(){
+	document.getElementById("imageID12").style.display = "block"
+}
+
+function onMarshmallowClick(){
+	document.getElementById("imageID13").style.display = "block"
+}
+
+function onTurkeyClick(){
+	document.getElementById("imageID14").style.display = "block"
+}
+
+function onHamClick(){
+	document.getElementById("imageID15").style.display = "block"
+}
